f6ddb83e14ccfda1de7a1bbd8002048e03a79c1c
Ire-loop <bhaveshdeshmukh109@outlook.com>
2026-05-14 15:06:08 +0530

    Rename `TaskTrackerApiApplication` to `TaskTrackerApplication`.

--- src/main/kotlin/com/example/taskTracker/TaskTrackerApplication.kt
+++ src/main/kotlin/com/example/taskTracker/TaskTrackerApplication.kt
@@ -4,8 +4,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 
 @SpringBootApplication
-class TaskTrackerApiApplication
+class TaskTrackerApplication
 
 fun main(args: Array<String>) {
-    runApplication<TaskTrackerApiApplication>(*args)
+    runApplication<TaskTrackerApplication>(*args)
 }
--- src/test/kotlin/com/example/taskTracker/TaskTrackerApplicationTests.kt
+++ src/test/kotlin/com/example/taskTracker/TaskTrackerApplicationTests.kt
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test
 import org.springframework.boot.test.context.SpringBootTest
 
 @SpringBootTest
-class TaskTrackerApiApplicationTests {
+class TaskTrackerApplicationTests {
 
     @Test
     fun contextLoads() {
